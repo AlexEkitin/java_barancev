89c372da636a3dceaba24217353bf73c62252c07
IuliiaPopova <popovasqa@gmail.com>
2016-10-20 20:25:18 -0700

    add several funtion

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Program.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Program.java
@@ -3,7 +3,32 @@ package ru.stqa.pft.sandbox;
 public class Program {
 
     public static void main(String[] args) {
+        hello("User");
+        hello("144");
+        double l = 5;
+        System.out.println("area = "+ l+ " = " + area(l));
 
-        System.out.println("Hello, world!");
+        double a = 5;
+        double b = 7;
+        System.out.println("area 2 = "+ a + " * "+b +" = " + area(a, b));
+
+        //double l=8;
+        //double s = l*l;
+        //System.out.println("squere " + l +" = " + s);
     }
+
+    public static void hello(String sombody){
+
+        System.out.println("Hello, "+sombody+"!");
+    }
+
+    public static double area(double len){
+        return len * len;
+    }
+
+    public static double area (double a, double b){
+        return a * b;
+    }
+
+
 }
