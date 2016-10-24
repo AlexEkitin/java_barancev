3d7332888be41c9a3468adc823b00600f4ea9e69
AlexEkinin <kiluy9iuoh@yandex.ru>
2016-10-24 12:16:45 -0700

    Home work 3

--- sandbox/src/test/java/ru/stqa/pft/sandbox/DistanceTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/DistanceTests.java
@@ -10,15 +10,17 @@ public class DistanceTests {
     public void testDistance1() {
         Point p1 = new Point(0, 0);
         Point p2 = new Point(3, 4);
+        
         Assert.assertEquals(p1.distance(p2), 5.0);
     }
+
 
     @Test
     public void testDistance2() {
         Point p1 = new Point(0, 0);
         Point p2 = new Point(0, 0);
+
         Assert.assertEquals(p1.distance(p2), 0.0);
     }
-
 
 }
