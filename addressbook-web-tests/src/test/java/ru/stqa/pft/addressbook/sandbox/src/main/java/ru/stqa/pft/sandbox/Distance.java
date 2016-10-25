66d2e9b81606fe753d24c59777366d61a2aef541
AlexEkinin <kiluy9iuoh@yandex.ru>
2016-10-25 14:30:49 -0700

    2.6

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/sandbox/src/main/java/ru/stqa/pft/sandbox/Distance.java
@@ -0,0 +1,15 @@
+package ru.stqa.pft.sandbox;
+
+public class Distance {
+
+    public static void main(String[] args) {
+
+        Point p1 = new Point(0, 0);
+        Point p2 = new Point(3, 4);
+
+
+        System.out.println("distance = "+ p1.distance(p2));
+
+    }
+
+}
