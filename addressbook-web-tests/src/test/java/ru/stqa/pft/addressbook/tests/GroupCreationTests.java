da85f68bfb29febf2cd1200a8a7ce4b9a0024284
AlexEkinin <kiluy9iuoh@yandex.ru>
2016-11-07 21:39:19 -0800

    4..9

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -4,7 +4,6 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
-import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -15,35 +14,16 @@ public class GroupCreationTests extends TestBase {
         // list of groups before
         app.getNavigationHelper().gotoGroupPage();
         List<GroupData> before = app.getGroupHelper().getGroupList();
-        GroupData group = new GroupData("test2", null, null);
+        GroupData group = new GroupData("test8", null, null);
         app.getGroupHelper().createGroup(group);
         // list of groups after
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size() + 1);
 
-        // compare I type
-        before.add(group);
-        int max = 0;
-        for (GroupData g : after){
-            if (g.getId()>max) {
-                max = g.getId();
-            }
-        }
-
-        // compare II type
-
         // compare 2 objects type GroupData (o1 and o2)
-        Comparator<? super GroupData> byId = new Comparator<GroupData>() {
-            @Override
-            public int compare(GroupData o1, GroupData o2) {
-                return Integer.compare(o1.getId(), o2.getId());
-            }
-        };
         // find max objects and get his Id
-        int max1 = after.stream().max(byId).get().getId();
-
-
-        group.setId(max1);
+        // transform from spisok to flow, find max element (use Id), get group with max id
+        group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
         before.add(group);
         // modify from lists to sets
         Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
