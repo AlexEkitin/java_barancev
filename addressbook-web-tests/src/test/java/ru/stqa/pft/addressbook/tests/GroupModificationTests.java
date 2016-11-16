1ce44f2b3945c9a80f0c7ffc0fb66937ef597ea7
AlexEkinin <kiluy9iuoh@yandex.ru>
2016-11-16 14:14:05 -0800

    5.2

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -1,6 +1,7 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
@@ -10,25 +11,26 @@ import java.util.List;
 
 public class GroupModificationTests extends TestBase {
 
-    @Test
-    public void testGroupModification() {
+    @BeforeMethod
+    public void ensurePreconditions(){
         app.getNavigationHelper().gotoGroupPage();
         if (! app.getGroupHelper().isThereAGroup()){
             app.getGroupHelper().createGroup(new GroupData("groupname1", "header1", "footer1"));
         }
+    }
+
+    @Test
+    public void testGroupModification() {
         List<GroupData> before = app.getGroupHelper().getGroupList();
-        app.getGroupHelper().selectGroup(before.size() - 1);
-        app.getGroupHelper().initGroupModification();
+        int index = before.size() - 1;
         //pri modifikacii gruppi mi ukazivaem novie dannie, a id ostavlaem starii
-        GroupData group = new GroupData(before.get(before.size() - 1).getId(), "groupname1", "header1", "footer1");
-        app.getGroupHelper().fillGroupForm(group);
-        app.getGroupHelper().submitGroupModification();
-        app.getGroupHelper().returnToGroupPage();
+        GroupData group = new GroupData(before.get(index).getId(), "groupname1", "header1", "footer1");
+        app.getGroupHelper().modifyGroup(index, group);
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(before.size(), after.size());
 
         //udaliaem staruu versiu elementa, kotorogo mi modificirovali na saite i...
-        before.remove(before.size() - 1);
+        before.remove(index);
         //...dobavim novii element, kotorii poyavitsa posle modifikacii s parametrami "group"
         before.add(group);
         //sravnivaem elementi spiska po id
